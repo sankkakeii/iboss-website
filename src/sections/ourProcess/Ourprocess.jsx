31b84e3876be3eedff8e7ce902c95bf52e62dab3
David Ikenna <abioduog@gmail.com>
2024-01-18 12:13:28 +0100

    add consistent font size and padding to components

--- src/sections/ourProcess/Ourprocess.jsx
+++ src/sections/ourProcess/Ourprocess.jsx
@@ -9,54 +9,54 @@ const Ourprocess = () => {
 
   return (
     <section id="ourprocess">
+      <h2>Our Process</h2>
+      <h5>Simple, Seamless, Streamlined</h5>
       <div className="container ourprocess_container">
-        <h2>Our Process</h2>
-        <h5>Simple,Seamless,Streamlined</h5>
-      </div>
-      <div className="ourprocess_card">
-        <div className="the_cards">
-          <Card>
-            <img src={Discover} alt="" />
-            <h6>Discover</h6>
-            <p>
-              We start with a discussion with you so we can structure team,
-              approach, success metrics, timescale, budget and delegate required
-              skill sets to the task
-            </p>
-          </Card>
-        </div>
-        <div className="the_cards">
-          <Card>
-            <img src={Design} alt="" className="ourprocess_card_img" />
-            <h6>Design</h6>
-            <p>
-              Our design strategy flows from discovery through a user-focused
-              mind-map to create a total end-to-end deeply satisfying
-              experience.
-            </p>
-          </Card>
-        </div>
-        <div className="the_cards">
-          <Card>
-            <img src={Develop} alt="" />
-            <h6>Develop</h6>
-            <p>
-              Our team of developers consider such issues as protocols,
-              compatibility, user accessibility and security to code with
-              precision and functionality
-            </p>
-          </Card>
-        </div>
-        <div className="the_cards">
-          <Card>
-            <img src={Deploy} alt="" />
-            <h6>Deploy</h6>
-            <p>
-              The end goal is to put a software or an app in the hand of the
-              user, while yet gathering user feedback for post deployment
-              upgrades
-            </p>
-          </Card>
+        <div className="ourprocess_card">
+          <div className="the_cards">
+            <Card>
+              <img src={Discover} alt="" />
+              <h6>Discover</h6>
+              <p>
+                We start with a discussion with you so we can structure team,
+                approach, success metrics, timescale, budget and delegate required
+                skill sets to the task
+              </p>
+            </Card>
+          </div>
+          <div className="the_cards">
+            <Card>
+              <img src={Design} alt="" className="ourprocess_card_img" />
+              <h6>Design</h6>
+              <p>
+                Our design strategy flows from discovery through a user-focused
+                mind-map to create a total end-to-end deeply satisfying
+                experience.
+              </p>
+            </Card>
+          </div>
+          <div className="the_cards">
+            <Card>
+              <img src={Develop} alt="" />
+              <h6>Develop</h6>
+              <p>
+                Our team of developers consider such issues as protocols,
+                compatibility, user accessibility and security to code with
+                precision and functionality
+              </p>
+            </Card>
+          </div>
+          <div className="the_cards">
+            <Card>
+              <img src={Deploy} alt="" />
+              <h6>Deploy</h6>
+              <p>
+                The end goal is to put a software or an app in the hand of the
+                user, while yet gathering user feedback for post deployment
+                upgrades
+              </p>
+            </Card>
+          </div>
         </div>
       </div>
       <div className="btn_process">
