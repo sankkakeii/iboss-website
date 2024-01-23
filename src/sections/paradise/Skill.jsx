008635637db3a2f88b812aa75d683edb87e9d831
David Ikenna <sankkakeii@gmail.com>
2024-01-23 16:48:46 +0100

    add paradise section

---
+++ src/sections/paradise/Skill.jsx
@@ -0,0 +1,29 @@
+import "./paradise.css";
+
+const SkillBased = () => {
+
+    return (
+            <div className="skill_container">
+
+                <div className="skill_list">
+                    <ul className="skills_list_1">
+                        <li>Python</li>
+                        <li>React.Js</li>
+                        <li>Node.Js</li>
+                        <li>Ruby on Rails</li>
+                    </ul>
+                </div>
+
+                <div className="skills_list">
+                    <ul className="skills_list_2">
+                        <li>Dynamic 365</li>
+                        <li>Flutter</li>
+                        <li>Angular</li>
+                        <li>Swift</li>
+                    </ul>
+                </div>
+            </div>
+    );
+};
+
+export default SkillBased;
