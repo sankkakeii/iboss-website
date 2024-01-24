9dde07e324e67b917a8786cb9f56100b93be63db
ghostronin <osinachij161@gmail.com>
2024-01-24 13:34:35 +0100

    make change to routing for the navbar links

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -2,8 +2,7 @@ import "./navbar.css";
 import Logo from "../../assets/navbar/logo.svg";
 import { CiMail } from "react-icons/ci";
 import { NavLink } from "react-router-dom";
-import { HashLink } from 'react-router-hash-link';
-
+import { HashLink } from "react-router-hash-link";
 
 const Navbar = () => {
   return (
@@ -11,23 +10,31 @@ const Navbar = () => {
       {/* Container for the navigation elements */}
       <div className=" nav_container">
         {/* Logo section */}
-        <a href="/" className="nav_logo">
+        <HashLink smooth to="/#top" className="nav_logo">
           <img src={Logo} alt="Logo" />
-        </a>
+        </HashLink>
 
         {/* Navigation menu section */}
         <ul className="nav_menu">
           <li>
-            <NavLink to="/">Home</NavLink>
+            <HashLink smooth to="/#top" activeClassName="active">
+              Home
+            </HashLink>
           </li>
           <li>
-            <NavLink to="/about">About</NavLink>
+            <NavLink to="/about" activeClassName="active">
+              About
+            </NavLink>
           </li>
           <li>
-            <HashLink smooth to="/#our_works">Our Works</HashLink>
+            <HashLink smooth to="/#our_works" activeClassName="active">
+              Our Works
+            </HashLink>
           </li>
           <li>
-            <HashLink smooth to="/#services">Services</HashLink>
+            <HashLink smooth to="/#services" activeClassName="active">
+              Services
+            </HashLink>
           </li>
           {/* <a href="#our_works">Our Works</a>
           <a href="#services">Services</a> */}
