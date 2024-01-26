8d0919d0e54d119ca215210081ae6c364b377bd8
David Ikenna <sankkakeii@gmail.com>
2024-01-26 15:08:03 +0100

    improve animations for paradise section

--- src/sections/paradise/Role.jsx
+++ src/sections/paradise/Role.jsx
@@ -1,23 +1,42 @@
-import "./paradise.css";
+import React, { useState, useEffect } from "react";
+import "./scroll-role.css";
+
+const InfiniteScrolling = ({ items, fadeInOut = false, animationName }) => {
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % items.length);
+        }, 2000); // Adjust the interval as needed
+
+        return () => clearInterval(interval);
+    }, [items.length]);
+
+    return (
+        <ul className="skills_list">
+            {items.map((item, i) => (
+                <li
+                    key={i}
+                    className={fadeInOut ? (index === i ? "visible" : "hidden") : (i === 0 ? "visible" : "")}
+                    style={{ animationName }}
+                >
+                    {item}
+                </li>
+            ))}
+        </ul>
+    );
+};
 
 const RoleBased = () => {
-
     return (
         <div className="skill_container">
 
-            <div className="role_list">
-                <ul className="roles_list_1">
-                    <li>Fullstack Engineer</li>
-                    <li>Frontend Engineer</li>
-                </ul>
-
+            <div className="role_list1">
+                <InfiniteScrolling items={["Fullstack Engineer", "Frontend Engineer"]} animationName="fadeInOut" />
             </div>
 
-            <div className="role_list">
-                <ul className="roles_list_2">
-                    <li>Backend Engineer</li>
-                    <li>DevOps Engineer</li>
-                </ul>
+            <div className="role_list2">
+                <InfiniteScrolling items={["Backend Engineer", "DevOps Engineer"]} fadeInOut animationName="fadeInOut" />
             </div>
         </div>
     );
