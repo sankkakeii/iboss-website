387011e4505e575e0d7e0fb6aee1e6481ba78e42
David Ikenna <sankkakeii@gmail.com>
2024-01-26 14:34:24 +0100

    improve animations for paradise section

--- src/sections/paradise/Skill.jsx
+++ src/sections/paradise/Skill.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-// import "./paradise.css";
 import "./scroll.css";
 
 const skillsList1 = ["Python", "React.Js", "Node.Js", "Ruby on Rails"];
 const skillsList2 = ["Dynamic 365", "Flutter", "Angular", "Swift"];
 
-const InfiniteScrolling = ({ items }) => {
+const InfiniteScrolling = ({ items, fadeInOut = false, animationName }) => {
     const [index, setIndex] = useState(0);
 
     useEffect(() => {
@@ -18,35 +16,32 @@ const InfiniteScrolling = ({ items }) => {
     }, [items.length]);
 
     return (
-        <motion.ul
-            className="skills_list"
-            initial={{ opacity: 0, x: "100%" }}
-            animate={{ opacity: 1, x: "0%" }}
-            exit={{ opacity: 0, x: "-100%" }}
-            key={index} // Ensure a unique key for each render
-        >
+        <ul className="skills_list">
             {items.map((item, i) => (
-                <motion.li
+                <li
                     key={i}
-                    style={{ listStyleType: "none" }}
+                    className={fadeInOut ? (index === i ? "visible" : "hidden") : (i === 0 ? "visible" : "")}
+                    style={{ animationName }}
                 >
                     {item}
-                </motion.li>
+                </li>
             ))}
-        </motion.ul>
+        </ul>
     );
 };
 
 const SkillBased = () => {
     return (
         <div className="skill_container">
-            <AnimatePresence>
-                <InfiniteScrolling items={skillsList1} />
-                <InfiniteScrolling items={skillsList2} />
-            </AnimatePresence>
+            <div className="skillsList1">
+                <InfiniteScrolling items={skillsList1} animationName="scrollRightLeft1" />
+            </div>
+
+            <div className="skillsList2">
+                <InfiniteScrolling items={skillsList2} fadeInOut animationName="fadeInOut" />
+            </div>
         </div>
     );
 };
 
 export default SkillBased;
-
